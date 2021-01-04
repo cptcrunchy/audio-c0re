4f1d2b6ec7da5383d520d2f04b529397b5c9b452
Jason Gutierrez <jasong84@gmail.com>
2021-01-04 12:56:49 -0600

    Create POC

---
+++ src/pages/location/chicago.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ChicagoPage = () => {
+    return (
+        <div>
+            <h2>Chicago Page</h2>
+        </div>
+    )
+}
+
+export default ChicagoPage
---
+++ src/pages/location/denver.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const DenverPage = () => {
+    return (
+        <div>
+            <h2>Denver Page</h2>
+        </div>
+    )
+}
+
+export default DenverPage
